abf8602b1a85f7bafbb39a0cb7434f7c9727ef3c
user <muratovbm2@gmail.com>
2023-03-12 18:29:59 +0600

    new

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import request
+
+from models import db, People, Position
+from utils import create_app, init_app, json_response
+
+app = create_app()
+init_app(app)
+
+
+@app.route('/peoples', methods=['GET', 'POST'])
+def peoples():
+    if request.method == 'POST':
+        people = People(
+            pname=request.json.get('name'),
+            email=request.json.get('email'),
+            phone=request.json.get('phone')
+        )
+
+        db.session.add(people)
+        db.session.commit()
+
+        return json_response(people)
+
+    elif request.method == 'GET':
+        peoples_list = db.session.query(People).all()
+
+        return json_response(peoples_list)
+
+@app.route('/positions', methods=['GET'])
+def positions():
+    positions_list = db.session.query(Position).all()
+
+    return json_response(positions_list)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ models.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    phone = db.Column(db.String(20), unique=True)
+
+    def __init__(self, pname, email, phone):
+        self.name = pname
+        self.email = email
+        self.phone = phone
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return f'{self.email}'
+
+
+class Position(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(512))
+
+
+    def __init__(self, title, description):
+        self.title = title
+        self.description = description
+
+
+class PeoplePosition(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    people_id = db.Column(db.Integer, db.ForeignKey(People.id))
+    position_id = db.Column(db.Integer, db.ForeignKey(Position.id))
